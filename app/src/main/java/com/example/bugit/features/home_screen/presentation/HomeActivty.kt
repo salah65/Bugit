08dd8b256d0b00f3ebbd284db0eb4b5cd7df97e5
salah mohamed <salahsamy512@gmail.com>
2024-05-11 22:02:25 -0500

    - enhance ui experience

--- app/src/main/java/com/example/bugit/features/home_screen/presentation/HomeActivty.kt
+++ app/src/main/java/com/example/bugit/features/home_screen/presentation/HomeActivty.kt
@@ -13,7 +13,9 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,7 +27,6 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -42,17 +43,24 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.FileProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.bugit.R
 import com.example.bugit.ui.theme.BugitTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -69,7 +77,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BugitTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    BugSubmissionScreen(activity = this)
+                    BugSubmissionScreen()
                 }
             }
         }
@@ -78,10 +86,15 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BugSubmissionScreen(activity: Context) {
+@Preview
+fun BugSubmissionScreen() {
     val viewModel = viewModel<HomeScreenViewModel>()
     val state by viewModel.state.collectAsState()
     val context = LocalContext.current
+    val stroke = Stroke(
+        width = 2f,
+        pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+    )
 
 
     val singlePhotoPickerLauncher = rememberLauncherForActivityResult(
@@ -146,17 +159,49 @@ fun BugSubmissionScreen(activity: Context) {
 
         )
 
-        AsyncImage(
-            model = state.selectedImageUri,
-            contentDescription = null, // Provide a content description if needed
+        Box(
             modifier = Modifier
-                .size(200.dp)
                 .padding(16.dp)
-                .clip(RoundedCornerShape(size = 5.dp))
-        )
+                .fillMaxSize()
+                .weight(1f)
+                .drawBehind {
+                    drawRoundRect(
+                        color = Color.LightGray,
+                        style = stroke,
+                        cornerRadius = CornerRadius(10f)
+                    )
+                }
+                .clickable {
+                    singlePhotoPickerLauncher.launch(
+                        PickVisualMediaRequest(
+                            ActivityResultContracts.PickVisualMedia.ImageOnly
+                        )
+                    )
+                }
+        ) {
 
-        Spacer(modifier = Modifier.weight(1f))
+            if (state.selectedImageUri == null) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_gallery),
+                    contentDescription = "Button Icon",
+                    modifier = Modifier
+                        .size(100.dp)
+                        .align(Alignment.Center)
+                )
+            } else {
+                AsyncImage(
+                    model = ImageRequest.Builder(context).data(state.selectedImageUri)
+                        .allowHardware(false)
+                        .build(),
+                    contentDescription = null, // Provide a content description if needed
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentScale = ContentScale.Fit,
 
+
+                    )
+            }
+        }
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -167,9 +212,9 @@ fun BugSubmissionScreen(activity: Context) {
             Button(
                 modifier = Modifier.fillMaxWidth(),
                 onClick = {
-                    captureScreenshot(activity)?.let {bitmap->
-                        saveBitmapToFile(bitmap, activity).let {uri->
-                           viewModel.onEvent(HomeScreenEvent.OnImageUriChanged(uri))
+                    captureScreenshot(context)?.let { bitmap ->
+                        saveBitmapToFile(bitmap, context).let { uri ->
+                            viewModel.onEvent(HomeScreenEvent.OnImageUriChanged(uri))
                         }
                     }
                 }
@@ -184,27 +229,6 @@ fun BugSubmissionScreen(activity: Context) {
                     Text(stringResource(R.string.take_a_screenshot))
                 }
             }
-            Button(
-                modifier = Modifier.fillMaxWidth(),
-                onClick = {
-                    singlePhotoPickerLauncher.launch(
-                        PickVisualMediaRequest(
-                            ActivityResultContracts.PickVisualMedia.ImageOnly
-                        )
-                    )
-                }
-            ) {
-                Row {
-                    Icon(
-                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_gallery),
-                        contentDescription = "Button Icon",
-                        tint = Color.White
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(stringResource(R.string.select_image_from_gallery))
-                }
-            }
-
         }
 
     }
@@ -234,17 +258,19 @@ fun captureScreenshot(context: Context): Bitmap? {
 }
 
 fun saveBitmapToFile(bitmap: Bitmap, context: Context): Uri? {
-    return try {val imagesDir = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "my_images")
-    if (!imagesDir.exists()) {
-        imagesDir.mkdirs() // Create directory if it doesn't exist
-    }
-    val file = File(imagesDir, "image_${System.currentTimeMillis()}.png")
-    // Compress bitmap to PNG format (you can change PNG to JPEG or WEBP)
-    val os = FileOutputStream(file)
-    bitmap.compress(Bitmap.CompressFormat.PNG, 100, os)
-    os.flush()
-    os.close()
-    FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", file)
+    return try {
+        val imagesDir =
+            File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "my_images")
+        if (!imagesDir.exists()) {
+            imagesDir.mkdirs() // Create directory if it doesn't exist
+        }
+        val file = File(imagesDir, "image_${System.currentTimeMillis()}.png")
+        // Compress bitmap to PNG format (you can change PNG to JPEG or WEBP)
+        val os = FileOutputStream(file)
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, os)
+        os.flush()
+        os.close()
+        FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", file)
     } catch (e: IOException) {
         e.printStackTrace()
         // Handle file IO exception
